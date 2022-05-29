864911731555eed5eadce83869de21ee66b8953a
Joaquin Navarro <alfian1991@gmail.com>
2022-05-29 11:23:30 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,43 @@
+const app = require('express')();
+const fs = require('fs');
+const hls = require('hls-server');
+const { default: Hls } = require('hls.js');
+
+const hlsC = require('hls.js');
+
+app.get('/', (req, res) => {
+    return res.status(200).sendFile(`${__dirname}/client.html`);
+});
+
+// const h = new Hls({startPosition: 30});
+
+
+const server = app.listen(3000);
+
+new hls(server, {
+    provider: {
+        exists: (req, cb) => {
+            const ext = req.url.split('.').pop();
+
+            if (ext !== 'm3u8' && ext !== 'ts') {
+                return cb(null, true);
+            }
+
+            fs.access(__dirname + req.url, fs.constants.F_OK, function (err) {
+                if (err) {
+                    console.log('File not exist');
+                    return cb(null, false);
+                }
+                cb(null, true);
+            });
+        },
+        getManifestStream: (req, cb) => {
+            const stream = fs.createReadStream(__dirname + req.url);
+            cb(null, stream);
+        },
+        getSegmentStream: (req, cb) => {
+            const stream = fs.createReadStream(__dirname + req.url);
+            cb(null, stream);
+        }
+    }
+});
